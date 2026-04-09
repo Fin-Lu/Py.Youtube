586a1e5e9c633bf6fd2f64f83ea8cd74187c1d18
Fin-Lu <lucassenfin@icloud.com>
2026-04-09 13:13:17 +0200

    YouTube Downloader funktoniert jetzt geht es darum das er die Dateine auch ordnet

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-# Hey! Hier werden bald die ersten Zeilen stehen von meinem Python YouTube-Downloa0der, der für iOS optimiert ist und Metadaten entfernt.
-print("Hallo welt")
+# YouTube-Downloader | Python 3.12
+# Autor: Fin
+# Siehe "Erklärung" für Bedienungsanleitung und Voraussetzungen
+
+
+import yt_dlp
+
+def Download (url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        # '/Deine Pfade/%(title)s.%(ext)s'
+        'outtmpl': '%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'keepvideo': False,
+        'postprocessor_args': ['-vn'],
+
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Die MP3 wird geladen! ")
+    except Exception as e:
+        print(f"Beim Download ist ist ein Fehler aufgetreten: {e}")
+
+url = input("Bitte geben sie den Youtube Link ein (URL):  ")
+Download(url)
