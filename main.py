91939db8fdfefaaa65f298b7a445d4d6c1de0ec6
Fin-Lu <lucassenfin@icloud.com>
2026-04-09 11:50:31 +0200

    Anfang meines neuen projekt

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-# Hey! Hier werden bald die ersten Zeilen stehen von meinem Python YouTube-Downloader, der für iOS optimiert ist und Metadaten entfernt.
+# Hey! Hier werden bald die ersten Zeilen stehen von meinem Python YouTube-Downloa0der, der für iOS optimiert ist und Metadaten entfernt.
+print("Hallo welt")
